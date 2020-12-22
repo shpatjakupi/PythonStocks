07a6d634e4062d13d23db1cbe2206e7df5cc9349
Shpat Jakupi <shpatjakupi@Shpats-MacBook-Pro.local>
2020-12-22 17:06:47 +0100

    fixed api's with a search button to look up after a specific stock

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about.html', views.about, name="about"),
+    path('add_stock.html', views.add_stock, name="about", name="add_stock"),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -2,20 +2,24 @@ from django.shortcuts import render
 
 
 def home(request):
-    # pk_76c76fe8830641698a54009a397061fe
     import requests
     import json
 
-    api_request = requests.get(
-        "https://sandbox.iexapis.com/stable/stock/AAPL/quote?token=Tpk_1ffd0fa5cbb241889288941e2f4e0e0f")
-
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error ... "
-
-    return render(request, 'home.html', {'api': api})
+    if request.method == "POST":
+        ticker = request.POST["ticker"]
+        api_request = requests.get(
+            "https://sandbox.iexapis.com/stable/stock/"
+            + ticker
+            + "/quote?token=Tpk_1ffd0fa5cbb241889288941e2f4e0e0f"
+        )
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, "home.html", {"api": api})
+    else:
+        return render(request, "home.html", {"ticker": "Enter a ticker symbol above"})
 
 
 def about(request):
-    return render(request, 'about.html', {})
+    return render(request, "about.html", {})
