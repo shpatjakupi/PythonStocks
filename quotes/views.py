2a168a8a880ef7d0913e13373e0a8bc602a4f4a1
Shpat Jakupi <shpatjakupi@Shpats-MacBook-Pro.local>
2020-12-22 02:46:42 +0100

    form post etc

--- quotes/views.py
+++ quotes/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    # pk_76c76fe8830641698a54009a397061fe
+    import requests
+    import json
+
+    api_request = requests.get(
+        "https://sandbox.iexapis.com/stable/stock/AAPL/quote?token=Tpk_1ffd0fa5cbb241889288941e2f4e0e0f")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error ... "
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
