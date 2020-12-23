c113fc84e158f8ad98412ad79045f8c6de19d876
Shpat Jakupi <shpatjakupi@Shpats-MacBook-Pro.local>
2020-12-23 22:56:54 +0100

    delete stock now possible

--- quotes/views.py
+++ quotes/views.py
@@ -61,4 +61,9 @@ def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
     item.delete()
     messages.success(request, ("Stock Has Been Deleted!"))
-    return redirect(add_stock)
+    return redirect(delete_stock)
+
+
+def delete_stock(request):
+    ticker = Stock.objects.all()
+    return render(request, "delete_stock.html", {"ticker": ticker})
